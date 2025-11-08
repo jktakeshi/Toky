3bbe1039085fb34acee2342e0c315df9680cd2c6
Electross <jarrettdh24@gmail.com>
2025-11-08 11:57:39 -0500

    setup interview page basics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
+"use client";
+
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
@@ -58,6 +63,11 @@ export default function Home() {
           >
             Documentation
           </a>
+          <button
+            onClick={() => router.push("/interview")}
+            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]">
+            Go to Interview
+          </button>
         </div>
       </main>
     </div>
